1dabd8a5e68a0ffa3526efbc351d08cefe47ad75
ogbondac <christian.ogbonda@accessbankplc.com>
2021-09-16 20:06:16 +0100

    update add token

--- src/main/java/com/zurum/jwtsecurityexample/api/UserController.java
+++ src/main/java/com/zurum/jwtsecurityexample/api/UserController.java
@@ -1,17 +1,34 @@
 package com.zurum.jwtsecurityexample.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zurum.jwtsecurityexample.dto.RoleToUserForm;
 import com.zurum.jwtsecurityexample.model.ApplicationUser;
 import com.zurum.jwtsecurityexample.model.Role;
 import com.zurum.jwtsecurityexample.service.ApplicationUserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -39,9 +56,56 @@ public class UserController {
 
     @PostMapping("/role/addRole")
     public ResponseEntity<?> addRoleTouUser(@RequestBody RoleToUserForm form) {
-
         userService.addRoleToUser(form.getEmail(),form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void getRefresh(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+
+            try {
+                String referesh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("thisisgonnabesomesecretman".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(referesh_token);
+                String username = decodedJWT.getSubject();
+
+                log.info("EMAIL >>>> {}",username);
+
+                ApplicationUser user = userService.getUser(username);
+
+                String access_token = JWT.create()
+                        .withSubject(user.getEmail())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access-token", access_token);
+                tokens.put("referesh_token", referesh_token);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            }catch (Exception exception) {
+                log.error("Error logging in: {}", exception.getMessage());
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(403);
+//                    response.sendError(403, exception.getMessage());
+
+                Map<String, String> error = new HashMap<>();
+                error.put("Error", exception.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
+
     }
 
 }
--- src/main/java/com/zurum/jwtsecurityexample/security/SecurityConfig.java
+++ src/main/java/com/zurum/jwtsecurityexample/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.zurum.jwtsecurityexample.security;
 
 import com.zurum.jwtsecurityexample.filter.CustomAuthenticationFilter;
+import com.zurum.jwtsecurityexample.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -33,11 +35,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login").permitAll();
+        http.authorizeRequests().antMatchers("/api/login", "/api/token/refresh").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
